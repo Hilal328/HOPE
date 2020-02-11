a529bca60af6cc9050cb67cc042467657bc6abd1
nbaktir <nurgulbaktir@gmail.com>
2020-02-10 21:36:58 -0600

    Personal details added

--- src/test/java/com/syntax/pages/PersonaDetailsPageElements.java
+++ src/test/java/com/syntax/pages/PersonaDetailsPageElements.java
@@ -1,5 +1,7 @@
 package com.syntax.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -54,6 +56,95 @@ public class PersonaDetailsPageElements extends CommonMethods{
 
 	@FindBy(id = "menu_pim_addEmployee")
 	public WebElement addEmpBtn;
+	
+	@FindBy(xpath = "//div[@id='profile-pic']/h1")
+	public WebElement profileName;
+	
+	@FindBy(xpath = "//input[@id='photofile']")
+	public WebElement chooseFilebtn;
+	
+	@FindBy(id="personal_txtEmpFirstName")
+	public WebElement firstName;
+	
+	@FindBy(id="personal_txtEmpMiddleName")
+	public WebElement middleName;
+	
+	@FindBy(id="personal_txtEmpLastName")
+	public WebElement lastName;
+	
+	@FindBy(id = "personal_txtEmployeeId")
+	public WebElement employeeId;
+	
+	@FindBy(id="personal_txtOtherID")
+	public WebElement otherId;
+	
+	@FindBy(id = "personal_txtLicenNo")
+	public WebElement driverLicense;
+	
+	@FindBy(id="personal_txtLicExpDate")
+	public WebElement LicExpDate;
+	
+	@FindBy(xpath ="//input[@id='personal_txtLicExpDate']/following-sibling::img")
+	public WebElement licExpDateCalendar;
+	
+	@FindBy(xpath="//select[@class='ui-datepicker-month']")
+	public WebElement selectMonth;
+	
+	@FindBy(xpath="//select[@class='ui-datepicker-year']")
+	public WebElement selectYear;
+	
+	@FindBy(xpath="//table[@class='ui-datepicker-calendar']/tbody/tr/td")
+	public WebElement date;
+	
+	@FindBy(id="personal_txtNICNo")
+	public WebElement SSN;
+	
+	@FindBy(id="personal_txtSINNo")
+	public WebElement SIN;
+	
+	@FindBy(xpath="//label[contains(@for,'personal_optGender')]")
+	public List<WebElement> gender;
+	
+	@FindBy(id="personal_cmbMarital")
+	public WebElement maritalStatus;
+	
+	@FindBy(id="personal_cmbNation")
+	public WebElement nationality;
+	
+	@FindBy(id="personal_DOB")
+	public WebElement DOB;
+	
+	@FindBy(id="//input[@id='personal_DOB']/following::img")
+	public WebElement DOBCalendar;
+	
+	@FindBy(id="personal_txtEmpNickName")
+	public WebElement nickName;
+	
+	@FindBy(xpath="//label[contains(@for,'personal_chkSmokeFlag')]")
+	public List<WebElement> smoker;
+	
+	
+	@FindBy(id="personal_txtMilitarySer")
+	public WebElement militaryService;
+	
+	@FindBy(id ="btnSave")
+	public WebElement edit_Save;
+	
+	@FindBy(xpath = " //input[@id='btnAddAttachment']")
+	public WebElement addAttachment;
+	
+	@FindBy(id="ufile")
+	public WebElement chooseFile;
+	
+	
+	@FindBy(id="txtAttDesc")
+	public WebElement comment;
+	
+	@FindBy(id="btnSaveAttachment")
+	public WebElement attachmentUpload;
+	
+	@FindBy(xpath = "//img[@id='empPic']")
+	public WebElement empPic;
 
 	public void navigateToEmployeeList() {
 		jsClick(pim);
